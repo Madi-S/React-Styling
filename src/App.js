103a8fd9dd2b48c708a7fd3fb3341ce07fb22f7b
Madi-S <khovansky99@gmail.com>
2021-08-16 18:18:53 +0600

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -13,14 +13,55 @@ import Box3 from './Framer/Box3'
 import Box4 from './Framer/Box4'
 import Box5 from './Framer/Box5'
 
+import styled, { css } from 'styled-components'
+
+const Wrapper = styled.section`
+    padding: 4rem;
+    margin: 2rem;
+    background-color: ${({ bg }) => (bg ? bg : 'papayawhip')};
+    ${({ is3D }) =>
+        is3D &&
+        css`
+            background-color: white !important;
+            box-shadow: 1rem 1rem 2rem black !important;
+        `}
+`
+
+const Title = styled.h1`
+    font-size: 1.5rem;
+    text-align: center;
+    color: palevioletred;
+`
+
+const CircleWrapper = styled(Wrapper)`
+    border-radius: 100%;
+    width: 50rem;
+    height: 50rem;
+    margin: 0 auto;
+    text-align: center;
+`
+
 function App() {
-    return <div className='app'>
-        <Box1 />
+    return (
+        <div className='app'>
+            {/* <Box1 />
         <Box2 />
         <Box3 />
         <Box4 />
-        <Box5 />
-    </div>
+        <Box5 /> */}
+            <header>
+                <Wrapper>
+                    <Title>Hello Styled-Components</Title>
+                </Wrapper>
+                <Wrapper bg='white'>
+                    <Title>Hello World</Title>
+                </Wrapper>
+                <Wrapper is3D={true}>
+                    <Title>3D aye</Title>
+                </Wrapper>
+                <CircleWrapper is3D={true}>I am a cube</CircleWrapper>
+            </header>
+        </div>
+    )
 }
-
 export default App
